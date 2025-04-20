158982a55d22270059a61a66beb698ba589356bf
shusaku009 <shusaku009@gmail.com>
2025-04-20 20:54:49 +0900

    add: 型変換

--- script/function.go
+++ script/function.go
@@ -4,4 +4,18 @@ func main() {
 	var f float64 = 10
 	var n int = int(f)
 	println(n)
+
+	var sum float64
+	sum = 5 + 6 + 3
+	avg := sum / 3
+	if avg > 4.5 {
+		println("good")
+	}
+
+	var a, b, c bool
+	if a && b || !c {
+		println("true")
+	} else {
+		println("false")
+	}
 }
