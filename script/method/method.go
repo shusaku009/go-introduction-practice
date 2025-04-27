5841cbe19f267602452b8285d9456710c66606ad
shusaku009 <shusaku009@gmail.com>
2025-04-27 23:03:14 +0900

    add: レシーバ

--- script/method/method.go
+++ script/method/method.go
@@ -3,12 +3,19 @@ package main
 import "fmt"
 
 type Hex int
+type T int
 
 func (h Hex) String() string {
 	return fmt.Sprintf("%x", int(h))
 }
 
+func (t *T) f() { println("hi") }
+
 func main() {
 	var hex Hex = 100
 	fmt.Println(hex.String())
+
+	var v T
+	(&v).f()
+	v.f()
 }
