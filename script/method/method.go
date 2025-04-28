3e6eac3b34c09b94a67db67f7eb88f294afcbb1e
shusaku009 <shusaku009@gmail.com>
2025-04-28 09:54:51 +0900

    add: メソッド式

--- script/method/method.go
+++ script/method/method.go
@@ -15,8 +15,12 @@ func (t *T) f() { println("hi") }
 
 func main() {
 	var hex Hex = 100
-	f := hex.String
-	fmt.Println(f())
+	// メソッド値
+	// f := hex.String
+	// fmt.Println(f())
+	// メソッド式
+	f := Hex.String
+	fmt.Printf("%T\n%s\n", f, f(hex))
 	fmt.Println(hex.String())
 
 	var v T
