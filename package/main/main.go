c516d87d766b7c8dc2ca89eeee16e3ea4ea7b753
shusaku009 <shusaku009@gmail.com>
2025-04-29 09:27:27 +0900

    add: パッケージ変数

--- package/main/main.go
+++ package/main/main.go
@@ -5,7 +5,15 @@ import (
 	"go-introduction-practice/package/greeting"
 )
 
+var msg string = "hello"
+
+func f() { println(msg) }
+
 func main() {
 	txt := greeting.Do()
 	fmt.Println(txt)
+
+	f()
+	msg = "hi, gophers"
+	f()
 }
