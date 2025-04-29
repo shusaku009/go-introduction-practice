aac07b1329fecc5f796a0cc0f9f4c31e932e2c17
shusaku009 <shusaku009@gmail.com>
2025-04-29 09:32:50 +0900

    add: TRY ライブラリを取得する

---
+++ greeting/maing.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/tenntenn/greeting"
+
+func main() {
+	greeting.Do()
+}
--- script/main.go
+++ script/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Printf("Hello, 世界\n")
