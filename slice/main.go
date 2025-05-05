01f73861c8a8c01441c2d181ff08fb5118131cad
shusaku009 <shusaku009@gmail.com>
2025-05-05 18:16:35 +0900

    add: インターフェースの実装チェック

--- slice/main.go
+++ slice/main.go
@@ -1,5 +1,13 @@
 package main
 
+import "fmt"
+
+type Func func() string
+
+func (f Func) String() string { return f() }
+
+var _ fmt.Stringer = Func(nil)
+
 func main() {
 	ns := []int{1, 2, 3, 4}
 	// できない
