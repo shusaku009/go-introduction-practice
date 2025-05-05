a375d65a5f442d02d028b4653d99fbe59d4ba982
shusaku009 <shusaku009@gmail.com>
2025-05-05 17:04:10 +0900

    add: 関数にインターフェースを実装させる

---
+++ interface_func/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type Func func() string
+
+func (f Func) String() string {
+	return f()
+}
+
+func main() {
+	var s fmt.Stringer = Func(func() string {
+		return "hi"
+	})
+	fmt.Println(s)
+}
