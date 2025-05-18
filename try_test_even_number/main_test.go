b976a9a36e23166ca56e856bb22597c7915135d7
shusaku009 <shusaku009@gmail.com>
2025-05-18 12:22:17 +0900

    add: 標準入力のテストを追加

--- try_test_even_number/main_test.go
+++ try_test_even_number/main_test.go
@@ -20,3 +20,25 @@ func TestIsEven(t *testing.T) {
 		}
 	}
 }
+
+func TestProccessInput(t *testing.T) {
+	testcases := []struct {
+		input string
+		want bool
+	}{
+		{input: "1", want: false},
+		{input: "2", want: true},
+		{input: "3", want: false},
+		{input: "4", want: true},
+	}
+
+	for _, tt := range testcases {
+		got, err := proccessInput(tt.input)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if got != tt.want {
+			t.Errorf("processInput(%s) = %v, want %v", tt.input, got, tt.want)
+		}
+	}
+}
