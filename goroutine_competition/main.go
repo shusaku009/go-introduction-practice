d2a2d223e63c30eea193d488c142be6ee9b11b64
shusaku009 <shusaku009@gmail.com>
2025-05-24 18:39:57 +0900

    add: ゴールーチンの競合

---
+++ goroutine_competition/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	n := 1
+	go func() {
+		for i := 2; i <= 5; i++ {
+			fmt.Println(n, "*", i)
+			n *= i
+			time.Sleep(100)
+		}
+	}()
+
+	for i := 1; i <= 10; i++ {
+		fmt.Println(n, "+", i)
+		n += i
+		time.Sleep(100)
+	}
+}
