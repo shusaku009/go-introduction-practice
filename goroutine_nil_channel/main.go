1d1684d744bd63f2384ea2ab17637b4f0c8b66e7
shusaku009 <shusaku009@gmail.com>
2025-05-24 18:46:35 +0900

    add: nilチャネル

---
+++ goroutine_nil_channel/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch1 := make(chan int)
+	var ch2 chan string
+
+	go func() { ch1 <- 100 }()
+	go func() { ch2 <- "Hello" }()
+
+	select {
+	case v1 := <-ch1:
+		fmt.Println(v1)
+	case v2 := <-ch2:
+		fmt.Println(v2)
+	}
+}
