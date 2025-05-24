3f737afd0135dad5d3b51ceb666cb6e967738067
shusaku009 <shusaku009@gmail.com>
2025-05-24 18:46:08 +0900

    add: ゴールーチン チャネルの基本

---
+++ goroutine_channel/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := make(chan int)
+
+	go func() {
+		ch <- 100	
+	}()
+
+	go func() {
+		v := <-ch
+		fmt.Println(v)
+	}()
+
+	time.Sleep(2 * time.Second)
+}
