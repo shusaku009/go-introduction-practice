66616428b41cae5bc1a973b80ed77590090ba50f
shusaku009 <shusaku009@gmail.com>
2025-05-25 23:26:26 +0900

    add: 双方向チャネル

---
+++ bidirectional_channel/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func makeCh() chan int {
+	return make(chan int)
+}
+
+func recvCh(recv chan int) int {
+	go func() { recv <- 200 }()
+	return <-recv
+}
+
+func main() {
+	ch := makeCh()
+	go func() { ch <- 100 }()
+	fmt.Println(recvCh(ch))
+}
