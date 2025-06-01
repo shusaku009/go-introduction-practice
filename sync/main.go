adf3fa65a6489c3e274be39c42928cee5092bc29
shusaku009 <shusaku009@gmail.com>
2025-06-01 11:33:33 +0900

    add: sync ロック

---
+++ sync/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var m sync.Mutex
+	m.Lock()
+	go func() {
+		time.Sleep(3 * time.Second)
+		m.Unlock()
+		fmt.Println("unlock 1")
+	}()
+
+	m.Lock()
+	m.Unlock()
+	fmt.Println("unlock 2")
+}
